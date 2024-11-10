96965b03e903fae04e56cfb3a8168d563a9ad283
ThomazBrasil <80059179+ThomazBrasil@users.noreply.github.com>
2024-11-10 00:55:55 -0300

    Update UITest.py

--- UITest.py
+++ UITest.py
@@ -8,7 +8,7 @@ customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "gre
 
 app = customtkinter.CTk()
 app.geometry("400x780")
-app.title("CustomTkinter simple_example.py")
+app.title("From the screen to the ring to the pen to the king")
 
 
 
@@ -24,10 +24,11 @@ def slider_callback(value):
 frame_1 = customtkinter.CTkFrame(master=app)
 frame_1.pack(pady=20, padx=60, fill="both", expand=True)
 
-label = customtkinter.CTkLabel(text="Test")
+label = customtkinter.CTkLabel(master=frame_1, text="Test")
+label.pack(pady=10, padx=10)
 
 button_1 = customtkinter.CTkButton(master=frame_1, command=button_callback)
-button_1.pack(pady=10, padx=10)
+button_1.pack(pady=10, padx=10, side="left")
 
 slider_1 = customtkinter.CTkSlider(master=frame_1, command=slider_callback, from_=0, to=1)
 slider_1.pack(pady=10, padx=10)
@@ -38,8 +39,6 @@ text_1 = customtkinter.CTkTextbox(master=frame_1, width=200, height=70)
 text_1.pack(pady=10, padx=10)
 text_1.insert("0.0", "CTkTextbox\n\n\n\n")
 
-segmented_button_1 = customtkinter.CTkSegmentedButton(master=frame_1, values=["CTkSegmentedButton", "Value 2"])
-segmented_button_1.pack(pady=10, padx=10)
 
 
 app.mainloop()
